57917e7c9fa4f3233c0595d132ee437ff841c2a9
Eugene Kos <mstrsid@live.com>
2021-11-22 12:29:11 +0300

    fix speed slider on drag

--- src/js/script.js
+++ src/js/script.js
@@ -1,133 +1,135 @@
 ﻿$(document).ready(function () {
-/* owl start */
-$(".owl-carousel").owlCarousel({
-	items: 1,
-	loop: true,
-	nav: true,
-	navSpeed: 800,
-	dotsSpeed: 800,
-	margin: 200
-	/*responsive: {
-		300: {
-			items: 1,
-			nav: false,
-			margin: 50
-		},
-		550: {
-			items: 1,
-			margin: 50
-		},
-		600: {
-			items: 1,
-			margin: 50
-		},
-		700: {
-			items: 1,
-			margin: 100
-		},
-		800: {
-			items: 3,
-			margin: -50
-		}	,
-		900: {
-			items: 3,
-			margin: -50
-		}						
-	}*/
-});
-/* owl end */
+	/* owl start */
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		nav: true,
+		navSpeed: 800,
+		dotsSpeed: 800,
+		margin: 200,
+		smartSpeed: 800,
+		fluidSpeed: 800
+		/*responsive: {
+			300: {
+				items: 1,
+				nav: false,
+				margin: 50
+			},
+			550: {
+				items: 1,
+				margin: 50
+			},
+			600: {
+				items: 1,
+				margin: 50
+			},
+			700: {
+				items: 1,
+				margin: 100
+			},
+			800: {
+				items: 3,
+				margin: -50
+			}	,
+			900: {
+				items: 3,
+				margin: -50
+			}						
+		}*/
+	});
+	/* owl end */
 
-// 	/* validation form start */
-// 	function validateForm(form) {
-// 		$(form).validate({
-// 			rules: {
-// 				name: {
-// 					required: true,
-// 					minlength: 2,
-// 					lettersonly: true
-// 				},
-// 				phone: "required",
-// 				email: {
-// 					required: true,
-// 					email: true
-// 				},
-// 				message: {
-// 					required: true
-// 				}
-// 			},
+	// 	/* validation form start */
+	// 	function validateForm(form) {
+	// 		$(form).validate({
+	// 			rules: {
+	// 				name: {
+	// 					required: true,
+	// 					minlength: 2,
+	// 					lettersonly: true
+	// 				},
+	// 				phone: "required",
+	// 				email: {
+	// 					required: true,
+	// 					email: true
+	// 				},
+	// 				message: {
+	// 					required: true
+	// 				}
+	// 			},
 
-// 			messages: {
-// 				name: {
-// 					required: "Введите имя",
-// 					lettersonly: "Только буквы",
-// 					minlength: jQuery.validator.format("Не менее {0} символов"),
-// 				},
-// 				phone: "Введите свой номер телефона",
-// 				email: {
-// 					required: "Введите свой email",
-// 					email: "Неверный формат"
-// 				},
+	// 			messages: {
+	// 				name: {
+	// 					required: "Введите имя",
+	// 					lettersonly: "Только буквы",
+	// 					minlength: jQuery.validator.format("Не менее {0} символов"),
+	// 				},
+	// 				phone: "Введите свой номер телефона",
+	// 				email: {
+	// 					required: "Введите свой email",
+	// 					email: "Неверный формат"
+	// 				},
 
-// 				message: {
-// 					required: "Введите текст сообщения",
-// 					minlength: jQuery.validator.format("Не менее {0} символов"),
-// 				}
-// 			},
-// 		});
-// 	}
+	// 				message: {
+	// 					required: "Введите текст сообщения",
+	// 					minlength: jQuery.validator.format("Не менее {0} символов"),
+	// 				}
+	// 			},
+	// 		});
+	// 	}
 
-// 	function resetInput(form) {
-// 		$('.modal__close').on('click', function () {
-// 			var f = $(form);
-// 			f.validate().resetForm(); // clear out the validation errors
-// 			f[0].reset(); // clear out the form data
-// 		});
-// 	}
+	// 	function resetInput(form) {
+	// 		$('.modal__close').on('click', function () {
+	// 			var f = $(form);
+	// 			f.validate().resetForm(); // clear out the validation errors
+	// 			f[0].reset(); // clear out the form data
+	// 		});
+	// 	}
 
-// 	validateForm('#prices__form');
-// 	validateForm('#questions__form');
+	// 	validateForm('#prices__form');
+	// 	validateForm('#questions__form');
 
-// 	resetInput('#prices__form');
-// 	resetInput('#questions__form');
+	// 	resetInput('#prices__form');
+	// 	resetInput('#questions__form');
 
-// 	$('input[name=phone]').mask("+375(99)999-99-99");
-// 	/* validation form end */
+	// 	$('input[name=phone]').mask("+375(99)999-99-99");
+	// 	/* validation form end */
 
-// 	/* smooth scroll start*/
-// 	$('a').on('click', function (e) {
-// 		if (this.hash !== '') {
-// 			e.preventDefault();
-// 			const hash = this.hash;
-// 			$('html, body').animate({
-// 				scrollTop: $(hash).offset().top
-// 			}, 800);
-// 		}
-// 	});
-// 	/* smooth scroll end*/
+	// 	/* smooth scroll start*/
+	// 	$('a').on('click', function (e) {
+	// 		if (this.hash !== '') {
+	// 			e.preventDefault();
+	// 			const hash = this.hash;
+	// 			$('html, body').animate({
+	// 				scrollTop: $(hash).offset().top
+	// 			}, 800);
+	// 		}
+	// 	});
+	// 	/* smooth scroll end*/
 
-// 	/* mailer start*/
-// 	$('.modal__close').on('click', function () {
-// 		$('.overlay, #thankyou').fadeOut();
-// 	});
+	// 	/* mailer start*/
+	// 	$('.modal__close').on('click', function () {
+	// 		$('.overlay, #thankyou').fadeOut();
+	// 	});
 
-// 	$('form').submit(function (e) {
-// 		e.preventDefault();
-// 		if (!$(this).valid()) {
-// 			return;
-// 		}
-// 		$.ajax({
-// 			type: "POST",
-// 			url: "mailer/smart.php",
-// 			data: $(this).serialize()
-// 		}).done(function () {
-// 			$(this).find('input').val('');
-// 			$(this).find('textarea').val('');
-// 			$('.overlay, #thankyou').fadeIn();
-// 			$('form').trigger('reset');
-// 		});
-// 		return false;
-// 	});
-// 	/* mailer end */
+	// 	$('form').submit(function (e) {
+	// 		e.preventDefault();
+	// 		if (!$(this).valid()) {
+	// 			return;
+	// 		}
+	// 		$.ajax({
+	// 			type: "POST",
+	// 			url: "mailer/smart.php",
+	// 			data: $(this).serialize()
+	// 		}).done(function () {
+	// 			$(this).find('input').val('');
+	// 			$(this).find('textarea').val('');
+	// 			$('.overlay, #thankyou').fadeIn();
+	// 			$('form').trigger('reset');
+	// 		});
+	// 		return false;
+	// 	});
+	// 	/* mailer end */
 });
 
 // jQuery.validator.addMethod("lettersonly", function (value, element) {
